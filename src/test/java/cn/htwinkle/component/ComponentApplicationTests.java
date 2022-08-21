fbad7e2a8c26a82d8f1da3a98e17b58ecc18cce1
TWINKLE <zhanghjqqqq@163.com>
2022-08-21 11:39:22 +0800

    Initial commit

---
+++ src/test/java/cn/htwinkle/component/ComponentApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.htwinkle.component;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
